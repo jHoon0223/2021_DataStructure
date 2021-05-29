befd1f09bd686e40995475fcad64b986230eb258
jHoon0223 <aleejaehun@naver.com>
2021-05-29 23:35:38 +0900

    210529 : SelectionSort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,27 +1,5 @@
-public class Sorting {
-    static void InsertionSort(int[] a) {
-        InsertionSort(a, a.length);
-    }
-    static void InsertionSort(int[] a, int size) {
-        for (int i = 1; i < size; i++) {
-            int target = a[i];
-            int j = i-1;
-
-            while(j >= 0 && target < a[j]) {
-                a[j+1] = a[j];
-                j--;
-            }
-
-            a[j+1] = target;
-
-            System.out.print(i + "단계 : ");
-            for (int k = 0; k < size; k++)
-                System.out.print(a[k] + " ");
-            System.out.print("\n");
-        }
-    }
-
-    static void SelectionSort(int[] a) { InsertionSort(a, a.length); }
+public class SelectionSort {
+    static void SelectionSort(int[] a) { SelectionSort(a, a.length); }
     static void SelectionSort(int[] a, int size) {
         int cnt = 0;
 
@@ -52,10 +30,13 @@ public class Sorting {
     }
 
     public static void main(String[] args) {
-        int[] array = {10, 5, 3, 6, 8, 9};
+        int[] array = new int[32];      //배열 생성
+
+        for (int i = 0; i < 32; i++)
+            array[i] = (int)(Math.random() * 32);       //생성된 배열에 랜덤하게 수 삽입
 
         System.out.print("랜덤 생성 배열 : ");
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 32; i++)
             System.out.print(array[i] + " ");
         System.out.print("\n");     //처음 생성된 배열 출력
 
